949952cb3fec380e8ae968a808da7615c3b52b4d
VitoJan <56464847+vitojan@users.noreply.github.com>
2025-01-17 21:53:37 +0800

    Update web.py

--- web.py
+++ web.py
@@ -117,6 +117,6 @@ def img():
 
 if __name__ == '__main__':
     # Start the Flask application
-    info_update.start()
+    Thread(target=info_update).run()
     app.run(host='0.0.0.0', port=5000, threaded=True)
 
